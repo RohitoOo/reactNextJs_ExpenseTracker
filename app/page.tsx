1a01d382241360bad4c8b41e4c917e3df6768adb
RohitoOo <bhambhani.ba@gmail.com>
2024-08-02 17:47:44 -0400

    Guest Login Page Created

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
-const HomePage = () => {
-  return ( <main>
-    <h1>Expense Tracker</h1>
-  </main> );
-}
- 
+import Guest from "@/components/Guest";
+import { currentUser } from "@clerk/nextjs/server";
+
+const HomePage = async () => {
+  const user = await currentUser();
+
+  if (!user) {
+    return <Guest />;
+  }
+  return (
+    <main>
+      <br />
+      <br />
+      <h1>Welcome, {user.fullName}</h1>
+    </main>
+  );
+};
+
 export default HomePage;
