e942572ad57aaf4f2b5493a38928cb1dd57ec181
RohitoOo <bhambhani.ba@gmail.com>
2024-08-02 20:26:22 -0400

    Transaction Components Added

--- app/actions/addTransactions.ts
+++ app/actions/addTransactions.ts
@@ -1,38 +1,62 @@
 "use server";
+import { db } from "@/lib/db";
 
+import { auth } from "@clerk/nextjs/server";
+import { revalidatePath } from "next/cache";
 interface TransactionData {
-    text: string;
-    amount: number;
+  text: string;
+  amount: number;
 }
 
 interface TrasactionResult {
-    data?: TransactionData;
-    error?: string;
+  data?: TransactionData;
+  error?: string;
 }
 
 async function addTransaction(formData: FormData): Promise<TrasactionResult> {
-    const textValue = formData.get("text");
-    const amountValue = formData.get("amount");
+  const textValue = formData.get("text");
+  const amountValue = formData.get("amount");
 
-    // Check for input values
+  // Check for input values
 
-    if (!textValue || textValue === "" || !amountValue) {
-        return {
-            error: "Text / Amount is missing",
-        };
-    }
+  if (!textValue || textValue === "" || !amountValue) {
+    return {
+      error: "Text / Amount is missing",
+    };
+  }
 
-    const text: string = textValue.toString(); ///Ensure text is a string
-    const amount: number = parseFloat(amountValue.toString()); ///Parse amount as number
+  const text: string = textValue.toString(); ///Ensure text is a string
+  const amount: number = parseFloat(amountValue.toString()); ///Parse amount as number
 
-    const transactionData : TransactionData = {
+  // Get Logged in User
+
+  const { userId } = auth();
+
+  console.log({ userId });
+
+  // Check for User
+
+  if (!userId) {
+    return { error: "No User Found" };
+  }
+
+  try {
+    const transactionData: TransactionData = await db.transaction.create({
+      data: {
         text,
-        amount 
-    }
+        amount,
+        userId
+          },
+    });
+
+    revalidatePath('/')
 
     return {
-        data : transactionData
-    }
+      data: transactionData,
+    };
+  } catch (error) {
+    return {error : "Transaction Not Added"}
+  }
 }
 
 export default addTransaction;
--- components/AddTransaction.tsx
+++ components/AddTransaction.tsx
@@ -1,37 +1,37 @@
 "use client";
 
 import addTransaction from "@/app/actions/addTransactions";
+import {toast} from 'react-toastify';
 
-
-
-const AddTransaction =  () => {
+const AddTransaction = () => {
     const clientAction = async (formData: FormData) => {
         // console.log(formData.get('text'), formData.get('amount'))
 
-        const {data, error} = await addTransaction(formData);
+        const { data, error } = await addTransaction(formData);
 
-        if(error){
-            alert(error)
-        }else {
-            alert("Transaction Added");
+        if (error) {
+            toast.error(error)
+        } else {
+            // alert("Transaction Added");
+            toast.success("Transaction Added")
             console.log(data)
         }
 
     }
-    return ( 
+    return (
         <>
             <h3>Add Transaction</h3>
-            <form action={ clientAction }>
+            <form action={clientAction}>
                 <div className="form-control">
                     <label htmlFor="text">
                         Text
                     </label>
-                    <input type="text" id='text' name='text' placeholder="Enter Text..."/>
+                    <input type="text" id='text' name='text' placeholder="Enter Text..." />
                 </div>
-                
+
                 <div className="form-control">
                     <label htmlFor="amount">
-                        Amount <br/>
+                        Amount <br />
                         (negative - expense, positive - income)
                         <input type="number" name='amount' id='amount' placeholder="Enter Amount..." step="0.01" />
                     </label>
@@ -39,7 +39,7 @@ const AddTransaction =  () => {
                 <button className="btn">Add Transaction</button>
             </form>
         </>
-     );
+    );
 }
- 
+
 export default AddTransaction;
